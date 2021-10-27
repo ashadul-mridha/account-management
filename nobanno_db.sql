d775921addbde7b4479385b70f54470dfd69ed85
ashadul-mridha <ashadulislam708@gmail.com>
2021-10-27 21:18:02 +0600

    database added

--- nobanno_db.sql
+++ nobanno_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2021 at 07:52 PM
+-- Generation Time: Oct 27, 2021 at 05:08 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
